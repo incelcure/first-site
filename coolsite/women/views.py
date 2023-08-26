3ec2163e5e610ed0f620fe19777c20a2135ce5d6
gubaException <57546821+incelcure@users.noreply.github.com>
2023-08-26 18:40:11 +0300

    models and migrations

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
@@ -8,5 +8,18 @@ def index(request):
     return HttpResponse("Страница приложения women.")
 
 
-def categories(request):
-    return HttpResponse("<h1>Статьи по категориям</h1>")
+def categories(request, catid):
+    if request.GET:
+        print(request.GET)
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>{catid}</p>")
+
+
+def archive(request, year):
+    if int(year) > 2020:
+        return redirect('home', permanent=True)
+
+    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
