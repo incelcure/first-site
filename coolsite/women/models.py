4c987638f91ec29709009a4627d9571f24d0baaf
gubaException <57546821+incelcure@users.noreply.github.com>
2023-08-27 21:21:11 +0300

    add some urls, styles and templates

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 
 # Create your models here.
+from django.urls import reverse
+
 
 class Women(models.Model):
     title = models.CharField(max_length=255)
@@ -13,3 +15,6 @@ class Women(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'post_id': self.pk})
